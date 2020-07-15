7b0e9f7730c0d48afd04a4e3513a41d4517506c7
pafuentess <pafuentess@unal.edu.co>
2020-07-15 11:03:44 -0500

    fix condition

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -13,7 +13,7 @@ def matrix_shape(matrix):
 
 def add_matrices2D(mat1, mat2):
     """ doc """
-    if (matrix_shape(mat1) != matrix_shape(mat2)):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return (None)
 
     shape = matrix_shape(mat1)
