09ef86e0085cfc763ff63718af5a218c28e3521f
pafuentess <pafuentess@unal.edu.co>
2020-07-15 11:00:48 -0500

    remove an else

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -13,14 +13,14 @@ def matrix_shape(matrix):
 
 def add_matrices2D(mat1, mat2):
     """ doc """
-    if len(mat1) != len(mat2):
+    if (matrix_shape(mat1) != matrix_shape(mat2)):
         return (None)
-    else:
-        shape = matrix_shape(mat1)
-        add_array = []
-        for i in range(0, shape[0]):
-            new_row = []
-            for j in range(0, shape[1]):
-                new_row.append(mat1[i][j] + mat2[i][j])
-            add_array.append(new_row)
-        return (add_array)
+
+    shape = matrix_shape(mat1)
+    add_array = []
+    for i in range(0, shape[0]):
+        new_row = []
+        for j in range(0, shape[1]):
+            new_row.append(mat1[i][j] + mat2[i][j])
+        add_array.append(new_row)
+    return (add_array)
