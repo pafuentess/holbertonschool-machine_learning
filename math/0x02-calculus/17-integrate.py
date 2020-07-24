1a317cb78ca2da9dc91264a3e77939b8a84f6b3f
pafuentess <pafuentess@unal.edu.co>
2020-07-24 11:31:53 -0500

    constante

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -8,19 +8,17 @@ def poly_integral(poly, C=0):
         if len(poly) == 1 and poly[0] == 0:
             return C
 
-        new_poly = []
+        new_poly = [C]
         for i in range(len(poly)):
             if type(poly[i]) is int or type(poly[i]) is float:
-                new_poly.append(0)
+                pass
             else:
                 return None
-        new_poly.append(0)
-
         for i in range(0, len(poly)):
             operation = poly[i] / (i + 1)
             if operation.is_integer():
                 operation = int(operation)
-            new_poly[i + 1] = operation
+            new_poly.append(operation)
         return (new_poly)
     else:
         return None
