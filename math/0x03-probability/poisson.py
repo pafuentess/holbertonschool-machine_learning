538533e510d7e31a02d8e77470bc0466c3a0e03f
pafuentess <pafuentess@unal.edu.co>
2020-07-29 10:49:13 -0500

    0 condition

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -10,11 +10,11 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(sum(data) / len(data))
         else:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         self.euler = 2.7182818285
