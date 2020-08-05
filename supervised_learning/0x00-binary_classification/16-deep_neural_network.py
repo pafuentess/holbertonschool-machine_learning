54b14fdbc14216bf09f8e14fe820b7590853b3e8
pafuentess <pafuentess@unal.edu.co>
2020-08-05 13:52:09 -0500

    add condition

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -12,7 +12,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
@@ -22,7 +22,7 @@ class DeepNeuralNetwork:
         keyw = ""
         keyb = ""
         for i in range(self.L):
-            if type(layers[i]) is not int or layers[i] < 1:
+            if type(layers[i]) is not int or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
             w = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             b = np.zeros((layers[i], 1))
