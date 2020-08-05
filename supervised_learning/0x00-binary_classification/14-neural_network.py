980bbaf23cbb3b5cc5ac5c1c621b4d44c4727330
pafuentess <pafuentess@unal.edu.co>
2020-08-05 15:10:36 -0500

    modify condition

--- supervised_learning/0x00-binary_classification/14-neural_network.py
+++ supervised_learning/0x00-binary_classification/14-neural_network.py
@@ -91,13 +91,13 @@ class NeuralNetwork:
         """ doc """
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations < 1:
+        if iterations < 0:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha < 0:
             raise ValueError("alpha must be positive")
-        for i in range(iterations + 1):
+        for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
         return self.evaluate(X, Y)
