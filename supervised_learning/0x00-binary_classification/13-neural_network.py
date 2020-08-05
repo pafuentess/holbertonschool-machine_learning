f127d4d03fce1272a0293798f1a6ff4df2a004d2
pafuentess <pafuentess@unal.edu.co>
2020-08-05 15:02:41 -0500

    change order update

--- supervised_learning/0x00-binary_classification/13-neural_network.py
+++ supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -74,15 +74,15 @@ class NeuralNetwork:
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """ doc """
+        m = X.shape[1]
         dz2 = A2 - Y
-        dw2 = (1 / len(Y[0])) * np.matmul(dz2, A1.T)
-        db2 = (1 / len(Y[0])) * np.sum(dz2, axis=1, keepdims=True)
-
-        self.__W2 = self.__W2 - alpha * dw2
-        self.__b2 = self.__b2 - alpha * db2
-
+        dw2 = (1/m) * np.matmul(A1, dz2.T)
+        db2 = (1/m) * np.sum(dz2, axis=1, keepdims=True)
         dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
-        dw1 = (1 / len(Y[0])) * np.matmul(dz1, X.T)
-        db1 = (1 / len(Y[0])) * np.sum(dz1, axis=1, keepdims=True)
-        self.__W1 = self.__W1 - alpha * dw1
-        self.__b1 = self.__b1 - alpha * db1
+        dw1 = (1 / m) * np.matmul(X, dz1.T)
+        db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
+
+        self.__W1 = self.__W1 - (alpha * dw1).T
+        self.__b1 = self.__b1 - (alpha * db1)
+        self.__W2 = self.__W2 - (alpha*dw2).T
+        self.__b2 = self.__b2 - (alpha * db2)
