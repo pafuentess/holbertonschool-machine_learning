1614509eab774542edc00acecb8c460dd0c83d4e
pafuentess <pafuentess@unal.edu.co>
2020-08-05 14:02:12 -0500

    pep8

--- supervised_learning/0x00-binary_classification/21-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/21-deep_neural_network.py
@@ -12,7 +12,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) == 0::
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
