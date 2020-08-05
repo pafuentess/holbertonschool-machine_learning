287bac1da5c0c0c8684d1815ade836016a2ec1f7
pafuentess <pafuentess@unal.edu.co>
2020-08-05 13:41:18 -0500

    change an order

--- supervised_learning/0x00-binary_classification/13-neural_network.py
+++ supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -74,15 +74,17 @@ class NeuralNetwork:
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """ doc """
-        m = Y.shape[1]
+        m = X.shape[1]
         dz2 = A2 - Y
         dw2 = (1/m) * np.matmul(A1, dz2.T)
         db2 = (1/m) * np.sum(dz2, axis=1, keepdims=True)
+
         self.__W2 = self.__W2 - (alpha*dw2).T
         self.__b2 = self.__b2 - (alpha * db2)
-        dsigmoid = A1 * (1 - A1)
-        dz1 = np.multiply(np.matmul(self.__W2.T, dz2), dsigmoid)
-        dw1 = (1 / m) * np.matmul(X, dz1.T)
+
+        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
+        dw1 = (1 / m) * np.matmul(dz1, X.T)
         db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
+
         self.__W1 = self.__W1 - (alpha * dw1).T
         self.__b1 = self.__b1 - (alpha * db1)
