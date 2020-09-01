1aaaca5c8405508f284ecb4820a73c9406525708
pafuentess <pafuentess@unal.edu.co>
2020-09-01 09:51:49 -0500

    task 11

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" doc """
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """ doc """
+    JsonModel = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(JsonModel)
+    return None
+
+
+def load_config(filename):
+    """ doc """
+    with open(filename, 'r') as f:
+        JsonModel = K.models.model_from_json(f.read())
+    return None
