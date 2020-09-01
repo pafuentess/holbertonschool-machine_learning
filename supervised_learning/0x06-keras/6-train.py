b41a8d5e974d14108a38a25e5a68da0c67f60df8
pafuentess <pafuentess@unal.edu.co>
2020-09-01 09:30:42 -0500

    change for 7 way

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -4,21 +4,17 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size,
-                epochs, validation_data=None,
-                early_stopping=False, patience=0,
-                verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
     """ doc """
+    callback = []
 
     if early_stopping:
-        callback = [K.callbacks.EarlyStopping(monitor='val_loss',
-                    patience=patience)]
-        return network.fit(x=data, y=labels,
-                           batch_size=batch_size, epochs=epochs,
-                           verbose=verbose, shuffle=shuffle,
-                           validation_data=validation_data, callbacks=callback)
-    else:
-        return network.fit(x=data, y=labels,
-                           batch_size=batch_size, epochs=epochs,
-                           verbose=verbose, shuffle=shuffle,
-                           validation_data=validation_data)
+        callback.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                        patience=patience))
+
+    return network.fit(x=data, y=labels,
+                       batch_size=batch_size, epochs=epochs,
+                       verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data, callbacks=callback)
