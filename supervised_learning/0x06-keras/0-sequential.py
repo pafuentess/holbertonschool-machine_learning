7a6a11e8e330bcdee1f36119b424271d665b4c0d
pafuentess <pafuentess@unal.edu.co>
2020-09-01 09:03:18 -0500

    fix module import

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """ doc """
 
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
 
-    model = keras.Sequential()
-    regulize = keras.regularizers.l2(lambtha)
-    model.add(keras.layers.Dense(layers[0], activation=activations[0],
+    model = K.Sequential()
+    regulize = K.regularizers.l2(lambtha)
+    model.add(K.layers.Dense(layers[0], activation=activations[0],
                                  input_shape=(nx,),
                                  kernel_regularizer=regulize,
                                  name='dense'))
 
     for i in range(1, len(layers)):
-        model.add(keras.layers.Dropout(1 - keep_prob))
-        model.add(keras.layers.Dense(layers[i], activation=activations[i],
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(layers[i], activation=activations[i],
                   kernel_regularizer=regulize, name=('dense_' + str(i))))
 
     return (model)
