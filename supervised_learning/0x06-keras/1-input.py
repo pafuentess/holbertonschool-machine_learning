e9ec355b4d6cbc4342aa1f651d23f57eabf34a49
pafuentess <pafuentess@unal.edu.co>
2020-09-01 09:10:07 -0500

    import module

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """ doc """
 
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
 
-    inputs = keras.Input(shape=(nx,))
-    regulize = keras.regularizers.l2(lambtha)
-    output = keras.layers.Dense(layers[0], activation=activations[0],
-                                input_shape=(nx,),
-                                kernel_regularizer=regulize,
-                                name='dense')(inputs)
+    inputs = K.Input(shape=(nx,))
+    regulize = K.regularizers.l2(lambtha)
+    output = K.layers.Dense(layers[0], activation=activations[0],
+                            input_shape=(nx,),
+                            kernel_regularizer=regulize,
+                            name='dense')(inputs)
 
     for i in range(1, len(layers)):
-        drop = (keras.layers.Dropout(1 - keep_prob))(output)
-        output = keras.layers.Dense(layers[i], activation=activations[i],
-                                    kernel_regularizer=regulize,
-                                    name=('dense_' + str(i)))(drop)
+        drop = (K.layers.Dropout(1 - keep_prob))(output)
+        output = K.layers.Dense(layers[i], activation=activations[i],
+                                kernel_regularizer=regulize,
+                                name=('dense_' + str(i)))(drop)
 
-    model = keras.Model(inputs=inputs, outputs=output)
+    model = K.Model(inputs=inputs, outputs=output)
     return (model)
