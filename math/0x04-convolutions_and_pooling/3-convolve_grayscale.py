62c90e4a2dbca9f6d3359bc36cd3e1882b258026
pafuentess <pafuentess@unal.edu.co>
2020-09-03 14:37:55 -0500

    fix int

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -19,14 +19,14 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         Pw = padding[1]
 
     if (padding == 'same'):
-        Pw = (int(((layerW - 1) * stride[1]) + kernelW - layerW) / 2) + 1
-        Ph = (int(((layerH - 1) * stride[0]) + kernelH - layerH) / 2) + 1
+        Pw = int(((layerW - 1) * stride[1] + kernelW - layerW) / 2) + 1
+        Ph = int(((layerH - 1) * stride[0] + kernelH - layerH) / 2) + 1
 
     if (padding == 'valid'):
         Ph = 0
         Pw = 0
 
-    images = np.pad(images, [(0, 0), (Ph, Ph), (Pw, Pw)], 'constant', constant_values=0)
+    images = np.pad(images, [(0, 0), (Ph, Ph), (Pw, Pw)], constant_values=0)
 
     convolutedH = int(((layerH - kernelH + (2 * Ph)) / stride[0]) + 1)
     convolutedW = int(((layerW - kernelW + (2 * Pw)) / stride[1]) + 1)
