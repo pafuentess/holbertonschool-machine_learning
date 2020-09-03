6150cbb02fc7b36bd40ee753a96b5157e3b2b4e0
pafuentess <pafuentess@unal.edu.co>
2020-09-03 14:30:53 -0500

    fix pw

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -14,25 +14,17 @@ def convolve_grayscale_same(images, kernel):
     kernelH = kernel.shape[0]
     kernelW = kernel.shape[1]
 
-    Ph = int((kernelH - 1) / 2)
-    Pw = int((kernelW - 1) / 2)
-
-    if kernelH % 2 == 0:
-        Ph = int(kernelH / 2)
-
-    if kernelW % 2 == 0:
-        Pw = int(kernelW / 2)
-
-    convolutedH = layerH - kernelH + 1 + (2 * Ph)
-    convolutedW = layerW - kernelW + 1 + (2 * Pw)
+    Ph = int((kernelH) / 2)
+    Pw = int((kernelW) / 2)
 
     images = np.pad(images, [(0, 0), (Ph, Ph), (Pw, Pw)],
                     'constant', constant_values=0)
-    convolutedImage = np.zeros((layers, convolutedH, convolutedW))
+
+    convolutedImage = np.zeros((layers, layerH, layerW))
     Nlayer = np.arange(0, layers)
 
-    for i in range(convolutedH):
-        for j in range(convolutedW):
+    for i in range(layerH):
+        for j in range(layerW):
             val = np.sum(np.multiply(images[Nlayer, i:kernelH + i,
                          j:kernelW + j], kernel), axis=(1, 2))
             convolutedImage[Nlayer, i, j] = val
