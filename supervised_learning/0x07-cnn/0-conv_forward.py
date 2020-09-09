ffe0bb1d12d14909b04f1e71f4e5c0b03b75154d
pafuentess <pafuentess@unal.edu.co>
2020-09-09 13:39:16 -0500

    remove padding tuple

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -13,10 +13,6 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     Kh = W.shape[0]
     Kw = W.shape[1]
 
-    if (type(padding) == tuple):
-        Ph = padding[0]
-        Pw = padding[1]
-
     if (padding == 'same'):
         Pw = int(((w_prev - 1) * stride[1] + Kw - w_prev) / 2) + 1
         Ph = int(((h_prev - 1) * stride[0] + Kh - h_prev) / 2) + 1
@@ -37,7 +33,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
     for i in range(convolutedH):
         for j in range(convolutedW):
-            for k in range(W.shape[3]):              
+            for k in range(W.shape[3]):
                 val = np.sum(np.multiply(A_prev[Nlayer,
                                          i*stride[0]:Kh + (i*stride[0]),
                                          j*stride[1]:Kw + (j*stride[1])],
