8d23ffaf60d38a20512a1cbac4b036e000246b10
pafuentess <pafuentess@unal.edu.co>
2020-10-23 08:48:23 -0500

    5

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" doc """
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """ doc """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError('matrix must be a numpy.ndarray')
+
+    if len(matrix.shape) != 2 or len(matrix) != matrix.shape[1]:
+        return None
+
+    T = np.transpose(matrix)
+    if not np.array_equal(T, matrix):
+        return None
+
+    Ev, Ew = np.linalg.eig(matrix)
+
+    if all(Ev > 0):
+        return "Positive definite"
+    if all(Ev >= 0):
+        return "Posiitve semi-definite"
+    if all(Ev < 0):
+        return "Negative definite"
+    if all(Ev <= 0):
+        return "Negative semi-definite"
+    else:
+        return 'Indefinite'
