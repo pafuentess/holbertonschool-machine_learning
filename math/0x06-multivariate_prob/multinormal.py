3ed5a4ecef95a820bb1716ef63824193a8577fed
pafuentess <pafuentess@unal.edu.co>
2020-10-26 16:11:22 -0500

    fix multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python3
-""" doc """
 
 import numpy as np
 
 
 class MultiNormal:
     """ doc """
-
     def __init__(self, data):
-        """ doc """
-        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-            err = 'data must be a 2D numpy.ndarray'
-            raise TypeError(err)
-        d, n = data.shape
+        if type(data) != np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
 
-        if n < 2:
-            err = 'data must contain multiple data points'
-            raise ValueError(err)
+        if (data.shape[1] < 2):
+            raise ValueError("data must contain multiple data points")
 
-        self.mean = np.mean(data, axis=1).reshape(d, 1)
+        n = data.shape[1]
+        self.mean = np.mean(data, axis=1)
+        d = data.shape[0]
+        self.mean = self.mean.reshape(d, 1)
 
-        deviaton = data - self.mean
-        self.cov = np.matmul(deviaton, deviaton.T) / (n - 1)
+        X = data - self.mean
+
+        self.cov = (np.matmul(X, X.T)) / (n - 1)
 
     def pdf(self, x):
         """ doc """
         if not isinstance(x, np.ndarray):
-            err = 'x must be a numpy.ndarray'
-            raise TypeError(err)
+            raise TypeError('x must be a numpy.ndarray')
 
         d = self.cov.shape[0]
 
         if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
             raise ValueError('x must have the shape ({}, 1)'.format(d))
 
-        det = np.linalg.det(self.cov)
-        inv = np.linalg.inv(self.cov)
-        constant = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+        constant = 1 / np.sqrt(((2 * np.pi) ** d) * (np.linalg.det(self.cov)))
         neg_dev = -(x - self.mean).T
 
-        inner = np.matmul(neg_dev, inv)
+        inner = np.matmul(neg_dev,  np.linalg.inv(self.cov))
         half_dev = (x - self.mean) / 2
         outer = np.matmul(inner, half_dev)
         f = np.exp(outer)
         pdf = constant * f
+        pdf = pdf.reshape(-1)[0]
 
-        return pdf.reshape(-1)[0]
+        return pdf
