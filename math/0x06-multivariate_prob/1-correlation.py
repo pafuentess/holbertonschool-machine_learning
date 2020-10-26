d75bc2ce9cc3e6c9adaaef0c3cce057400013eaa
pafuentess <pafuentess@unal.edu.co>
2020-10-26 15:54:16 -0500

    fix condition

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -8,7 +8,7 @@ def correlation(C):
     """ doc """
     if type(C) != np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if (C.shape[0] != C.shape[1]):
+    if (C.shape[0] != C.shape[1] or len(C.shape) != 2):
         raise ValueError("C must be a 2D square matrix")
 
     var = np.diag(1 / np.sqrt(np.diag(C)))
