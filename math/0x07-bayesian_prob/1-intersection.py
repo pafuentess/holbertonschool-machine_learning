7ac03ec7b485965bc373cc9da0dba447f1aae4f3
pafuentess <pafuentess@unal.edu.co>
2020-10-30 10:40:52 -0500

    first commit

---
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" doc """
+
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """ doc """
+
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x <= 0:
+        m = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(m)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if (not isinstance(P, np.ndarray)) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if (not isinstance(Pr, np.ndarray)) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose([np.sum(Pr)], [1.])[0]:
+        raise ValueError("Pr must sum to 1")
+
+    num = np.math.factorial(n)
+    den = np.math.factorial(x) * np.math.factorial(n - x)
+
+    coeficient = num / den
+
+    likehood = coeficient * (P ** x) * ((1 - P) ** (n - x))
+
+    return likehood * Pr
---
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" doc """
+
+from scipy import special
+
+
+def posterior(x, n, p1, p2):
+    """ doc """
+    if type(n) is not int or n < 1:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        m = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(m)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if (not isinstance(p1, float)) or p1 < 0 or p1 > 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+    if (not isinstance(p1, float)) or p2 < 0 or p2 > 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+
+    f1 = x + 1
+    f2 = n - x + 1
+
+    acum1 = special.btdtr(f1, f2, p1)
+    acum2 = special.btdtr(f1, f2, p2)
+
+    return acum2 - acum1
