7951fbace0951c830f365edf97b10dae320c4f1a
pafuentess <pafuentess@unal.edu.co>
2020-11-19 10:40:07 -0500

    0

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" doc """
+
+import numpy as np
+
+
+class GaussianProcess:
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """ doc """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, Y_init)
+
+    def kernel(self, X1, X2):
+        """ doc """
+        facto1 = np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + factor1
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
