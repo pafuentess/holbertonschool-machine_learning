26e7c4dc404ae1898b2ecd8613f001cfdf3b4612
pafuentess <pafuentess@unal.edu.co>
2020-11-19 11:39:46 -0500

    2

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" doc """
+
+import numpy as np
+
+
+class GaussianProcess:
+    """ doc """
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """ doc """
+        factor1 = np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + factor1
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """ doc """
+        K_inv = np.linalg.inv(self.K)
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+
+        mu_s = K_s.T.dot(K_inv).dot(self.Y)
+        mu_s = np.reshape(mu_s, -1)
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+        sigma = np.diagonal(cov_s)
+
+        return mu_s, sigma
+
+    def update(self, X_new, Y_new):
+        """ doc """
+        self.X = np.concatenate((self.X, X_new[:, np.newaxis]), axis=0)
+        self.Y = np.concatenate((self.Y, Y_new[:, np.newaxis]), axis=0)
+        self.K = self.kernel(self.X, self.X)
