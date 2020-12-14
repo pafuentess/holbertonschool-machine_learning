ee550a4c8b5d20588779a6b348766f1db77570ff
pafuentess <pafuentess@unal.edu.co>
2020-12-14 10:31:57 -0500

    sara ciega

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -11,6 +11,7 @@ class WindowGenerator():
     def __init__(self, input_width, label_width, shift,
                  train_df, val_df, test_df,
                  label_columns=None):
+        """ doc """
         self.train_df = train_df
         self.val_df = val_df
         self.test_df = test_df
